f10bbb176675113cea9464b83538f827ffd4a153
Shane B <dotfortun@users.noreply.github.com>
2025-02-10 23:49:47 +0000

    Finished up the create routes.

--- src/api/routes.py
+++ src/api/routes.py
@@ -13,30 +13,85 @@ CORS(api)
 
 
 @api.route("/authors", methods=["GET"])
-def get_authors():
+def read_authors():
     authors = Author.query.all()
     return jsonify({
         "count": len(authors),
-        "authors": [author.serialize() for author in authors]
+        "authors": [author.serialize() for author in authors],
     })
 
 
 @api.route("/authors/<int:id>", methods=["GET"])
-def get_author(id: int):
+def read_author_by_id(id: int):
     author = Author.query.filter_by(id=id).first()
     return jsonify(author.serialize())
 
 
+@api.route("/authors/<string:name>", methods=["POST"])
+def create_author(name: str):
+    author = Author(name=name)
+    db.session.add(author)
+    db.session.commit()
+    db.session.refresh(author)
+    return jsonify(author.serialize())
+
+
 @api.route("/books", methods=["GET"])
-def get_books():
-    books = Book.query.all()
+def read_books():
+    # This endpoint lets you filter on the properties of
+    # books that come in via url search paramaters.
+    args = request.args
+    books = Book.query.filter_by(**args).all()
     return jsonify({
         "count": len(books),
-        "books": [book.serialize() for book in books]
+        "books": [book.serialize() for book in books],
     })
 
 
 @api.route("/books/<int:id>", methods=["GET"])
-def get_book(id: int):
+def read_book(id: int):
     book = Book.query.filter_by(id=id).first()
+    return jsonify(book.serialize())
+
+
+@api.route("/books", methods=["POST"])
+def create_book():
+    """
+    POST:
+    {
+        "title": str,
+        "author_id?": int,
+        "isbn10?": str,
+        "isbn13?": str,
+        "cover?": str,
+        "have_read?": bool,
+        "is_awesome?": bool,
+    }
+    """
+    # We extract data from the request body that
+    # is sent to our backend as JSON text.
+    body = request.get_json(force=True)
+
+    if "title" not in body.keys():
+        return jsonify(msg="Books need titles"), 400
+    
+    if body.get("isbn10") or body.get("isbn13"):
+        book = Book.query.filter_by(
+            isbn10=body.get("isbn10"),
+            isbn13=body.get("isbn13"),
+        ).first()
+
+        if book:
+            return jsonify(msg="ISBN already in databse."), 400
+
+    book = Book(**body)
+
+    # We're relating two objects.
+    if "author_id" in body.keys():
+        author = Author.query.filter_by(id=body["author_id"]).first()
+        book.author = author
+
+    db.session.add(book)
+    db.session.commit()
+    db.session.refresh(book)
     return jsonify(book.serialize())
